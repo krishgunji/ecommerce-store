7f2c7612741649a840c1164261295f700f990427
root <krishan.gunjyal@gmail.com>
2025-08-27 10:25:01 +0000

    Sample Node ecommerce store:testing

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
-require("dotenv/config");
 
-return mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("conected to db")
-);
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  throw new Error("MONGO_URI environment variable not set");
+}
+
+mongoose.connect(mongoUri)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
